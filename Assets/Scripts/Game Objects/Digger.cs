d3a88014486568c0625f1c4b1d29dfc9668af410
Shervin Ghazazani <shervinghazazani@gmail.com>
2013-11-14 13:44:15 -0500

    Added KBControllableGameObject

---
+++ Assets/Scripts/Game Objects/Digger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Digger : MonoBehaviour {
+
+    /// <summary>
+    /// Use this for initialization
+    /// </summary>
+	void Start () {
+	
+	}
+	
+	/// <summary>
+    /// Update is called once per frame
+	/// </summary>
+    void Update () {
+	
+	}
+
+
+}
---
+++ Assets/Scripts/Model Objects/KBControllableGameObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KBControllableGameObject : KBGameObject {
+
+    public Player attachedPlayer = null;
+
+	/// <summary>
+    /// Use this for initialization
+	/// </summary>
+	void Start () {
+	
+	}
+	
+	/// <summary>
+    /// Update is called once per frame
+	/// </summary>
+	void Update () {
+	
+	}
+
+    /// <summary>
+    /// Checks if a player is currently attached to the object
+    /// </summary>
+    /// <returns> Boolean, true if attached and false otherwise</returns>
+    public bool isPlayerAttached()
+    {
+        if (attachedPlayer != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
