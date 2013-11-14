a445a519b5f525706c28e05890382719d1a0de8e
Aaron Freedman <afreedman.soka@gmail.com>
2013-11-14 13:48:57 -0500

    Kaiju

--- Assets/Scripts/Game Objects/Kaiju.cs
+++ Assets/Scripts/Game Objects/Kaiju.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using KBConstants;
 
 public class Kaiju : MonoBehaviour {
 
-	// Use this for initialization
+    //private X attachedPlayer;
+    private int[] coreHealth; // red, green, blue, black
+    private enum KaijuState { idle };
+
 	void Start () {
-	
+        coreHealth = new int[4];
+
+        for (int i = 0; i < coreHealth.Length; i++)
+        {
+            coreHealth[i] = KaijuConstants.CORE_HEALTH_DEFAULT;
+        }
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void damageCore(int which, int amount) {
+        coreHealth[which] -= amount;
+
+        if (coreHealth[which] < 0)
+        {
+            coreHealth[which] = 0;
+        }
+    }
+    void activateAbilityOne() { }
+    void activateAbilityTwo() { }
+    void activateAbilityThree() { }
+    void activateAbilityFour() { }
 }
