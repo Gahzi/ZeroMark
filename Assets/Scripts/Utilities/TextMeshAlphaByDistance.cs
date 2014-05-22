1794cac10df3dbec7138877ca2bb50b02eee7649
Aaron Freedman <aefreedman@nyu.edu>
2014-05-22 14:58:42 -0400

    Credit screen

---
+++ Assets/Scripts/Utilities/TextMeshAlphaByDistance.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TextMeshAlphaByDistance : MonoBehaviour
+{
+
+    public TextMesh text;
+    public Color originalColor;
+
+    private void Start()
+    {
+        text = GetComponent<TextMesh>();
+        originalColor = text.color;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, Camera.main.transform.position) < 10)
+        {
+            text.color = originalColor;
+        }
+        else
+        {
+            text.color = Color.clear;
+        }
+    }
+}
